90a4b6c01537a7b153ad6e9adf61d9df482645e3
Michael Frank <frankmp@rose-hulman.edu>
2012-05-02 23:38:55 -0400

    Task 5 added games

--- ChatBot-Team08/Chatbot.rb
+++ ChatBot-Team08/Chatbot.rb
@@ -3,3 +3,8 @@ puts "What is your name?"
 name = gets.chomp
 puts "Hello, " + name + "!"
 
+puts "1) Global Thermonuclear War"
+puts "2) Not Global Thermonuclear War"
+puts "3) Chess"
+puts "4) GO"
+puts "5) Segmentation fault"
